2f9e57e8c43f50729e8257030aa2ec6e4009036a
Kumar Shivam <shivam@192.168.1.9>
2026-05-23 02:44:15 +0530

    added langgraph chatbot project with streamlit

---
+++ LANGGRAPH_CHATBOT/chatbot_backend.py
@@ -0,0 +1,31 @@
+from langgraph.graph import StateGraph, START, END
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import BaseMessage, HumanMessage
+from dotenv import load_dotenv
+from typing import TypedDict, Annotated
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph.message import add_messages
+
+load_dotenv()
+
+llm =ChatOpenAI(model= 'gpt-4o-mini')
+
+class ChatState(TypedDict):
+    messages : Annotated[list[BaseMessage],add_messages]
+
+def chat_node(state: ChatState):
+    messages = state['messages']
+    response = llm.invoke(messages)
+    return {'messages':[response]}
+
+checkpointer = InMemorySaver()
+graph = StateGraph(ChatState)
+
+graph.add_node('chat_node', chat_node)
+graph.add_edge(START,'chat_node')
+graph.add_edge('chat_node', END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
+
+
+
